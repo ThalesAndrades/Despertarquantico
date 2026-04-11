0213a250c1678f7b74b1bd071b08a2fac9ed0045
ThalesAndrades <thales@ailun.com.br>
2026-04-11 14:08:21 -0300

    fix: remove stray tokens from config/database.php

--- config/database.php
+++ config/database.php
@@ -4,13 +4,11 @@
  *
  * Reads credentials from /.env at runtime.
  */
-+
 $host = Env::require('DB_HOST');
 $dbname = Env::require('DB_NAME');
 $username = Env::require('DB_USER');
 $password = (string) Env::get('DB_PASS', '');
 $charset = (string) Env::get('DB_CHARSET', 'utf8mb4');
-+
 return [
     'host' => $host,
     'dbname' => $dbname,
